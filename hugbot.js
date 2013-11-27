7110b96ab3af954db9133d5572e263fcd9370ef2
Imogen <imogen.wentworth@gmail.com>
2013-11-27 00:40:26 -0800

    Streaming works

--- hugbot.js
+++ hugbot.js
@@ -4,69 +4,80 @@ var auth = require('./auth');
 var bot = new ntwitter(auth);
 
 // Get a random number from 1 to 4
-function getRandNum(){
+function getRandNum() {
   var number = Math.floor((Math.random()*4)+1);
   return number;
 }
 
 // Get a random pug image
-function getRandPug(){
+function getRandPug() {
+  // FIXME write function to get random pug pic
   pug = ['http://2.bp.blogspot.com/-xXmJvA0VzXQ/UiDFbfEG8bI/AAAAAAAAAKM/1_Q91xejslQ/s1600/Gadget_the_pug_expressive_eyes.jpg'];
-  // pug =
   return pug;
+}
+
+// Log errors
+function handleError(error) {
+  console.error('response status:', error.statusCode);
+  console.error('data:', error.data);
+}
+
+// FIXME work out how to move this into a single function
+function postTweet(text, params, callback) {
+  bot.updateStatus(text, params, function(error) {
+    console.log(tweet.text);
+    if (error) {
+      handleError(error);
+    }
+  });
 }
 
 // Get a stream of Tweets
 function startStreaming() {
   bot.stream('statuses/filter', { track: 'need a hug, want a hug, need hugs, want hugs' }, function(stream) {
     stream.on('data', function(tweet) {
-      console.log(tweet.text);
 
-      setInterval(function(){
-// Check Tweet for specific matching phrases as Twitter's Streaming API doesn't allow for this
+      // Check Tweet for specific matching phrases as Twitter's Streaming API doesn't allow for this
       if (tweet.text.match(/need\sa\shug|want\sa\shug|need\shugs|want\shugs/)) {
 
         var number = getRandNum();
-        var pug = getRandPug();
+        var pugPic = getRandPug();
 
-        if (number <= 0) {
+        // 75% chance of hug
+        if (number <= 3) {
 
           var hugsParams = {
-            status: 'HUGBOT SENDS HUGS',
+            // FIXME status for testing
+            status: 'HUGBOT SENDS HUGS' + number,
             // status: '@' + tweet.user.screen_name + ' HUGBOT SENDS HUGS',
             in_reply_to_status_id: tweet.id
           };
 
           bot.updateStatus('text', hugsParams, function(error) {
+            console.log(tweet.text);
             if (error) {
-              console.error(error);
+              handleError(error);
             }
           });
 
+        // 25% chance of pug
         } else {
 
           var pugsParams = {
-            status: 'internet hugs are hard, but here\'s a pug instead ' + pug,
-            // status: '@' + tweet.user.screen_name + ' internet hugs are difficult, but here\'s a pug instead ' + pug,
+            // FIXME status for testing
+            status: ' internet hugs are difficult, but luckily pugs are plentiful ' + pugPic,
+            // status: '@' + tweet.user.screen_name + ' internet hugs are difficult, but luckily pugs are plentiful ' + pug,
             in_reply_to_status_id: tweet.id
           };
+
           bot.updateStatus('text', pugsParams, function(error) {
+            console.log(tweet.text);
             if (error) {
-              console.error(error);
+              handleError(error);
             }
           });
-
         }
-
       }
-
-      console.log('Starting new cycle');
-
-      }, 1000 * 60);
-
-
-
-
     });
   console.log('Listening for Tweets...');
   });
